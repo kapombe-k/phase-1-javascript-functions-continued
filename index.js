bf0f5f0f2135f7795bd9a5715937fb2c939f7f37
Eugene Kapombe <kapombek@gmail.com>
2025-03-13 21:59:27 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = (shughli='go to the office') => {
+    return `This Monday, I will ${shughli}.`
+}
+
+const wrapAdjective = function (style="*"){
+    return function (adjective = 'special') {
+        return `You are ${style}${adjective}${style}!`;
+    };
+}
